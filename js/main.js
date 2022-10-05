42152ec2a2083c445fbdfb1ae417b9e79d1f579a
Agustin <agustinfasulo2013@gmail.com>
2022-10-05 01:26:24 -0300

    Inicializacion del repo

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// alert("LA CONCHA DE TU MADRE ALL BOYS")
+
+// Argentina compite por el grupo "C" en la copa mundial de la FIFA 2022... compuesto también por
+// Arabia, Dinamarca, y Mexico
+
+// La idea es representar la competencia en ese grupo, realizando 3 partidos con aleatorios
+// en el que basandose en las caracteristicas de cada equipo (ataque, defensa, etc),
+// se defina esa "probabilidad" de ganar de cada equipo. Se realizan 3 rondas en las que se
+// determina una maxima cantidad de goles (4), y en base a eso se asignan los puntos. 
+// Despues de esas 3 "rondas" se define que equipo en base a los puntos, 
+// pasa a la siguiente ronda. Si hay un empate en puntos, dependiendo la cantidad de goles. 
+
+// Se pediría en un principio, el nombre de la persona a participar, y arrancaria con la ronda 1.
+// Se van mostrando los resultados de las rondas, a medida que avanza el simulador.
+
